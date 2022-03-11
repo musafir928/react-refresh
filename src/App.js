1bab5ff2f13a6644b345ce2a572b0a20e6926616
musafir928 <uganoghli@gmail.com>
2022-03-11 14:36:20 +0100

    add property type check

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import { useState } from 'react';
 import './App.scss';
+import Title from './components/Title';
 
 function App() {
-  const [name, setName] = useState("mario")
   const [showTitle, setShowTitle] = useState(true)
   const [events, setEvents] = useState([
 	  {title: "football",id:1},
@@ -10,7 +10,6 @@ function App() {
 	  {title: "basketball",id:3}
   ])
 
-  const handleClick = () => setName("luigi")
   const addEvent = () => setEvents(p=> [ ...p,{id: p.length+1,title: "new event"}])
   const handleDelete = (i) => setEvents(p=>p.filter(e=>e.id!==i))
   const toggleTitle = () => setShowTitle(p=> !p)
@@ -19,19 +18,19 @@ function App() {
 	   <button className="btn btn-primary mb-5" onClick={toggleTitle}>{showTitle ? "hide title" : "show title"}</button>
 	   {
 		   showTitle && <>    
-					<h1>Hi my name is {name}</h1>
-					<button className="btn btn-warning mb-3" onClick={handleClick}>Change name</button>
+					<Title
+					title="Mario Kingdom Events"
+					subtitle="All the latest events in mario kingdom" 
+					/>
+					<Title />
 				</>
 	   }
 	  <ul className='list-group'>
 		  {events.map(({title, id},i)=>(
-			  <>
 				<li className='list-group-item d-flex justify-content-around' key={i+1}>
 					{i+1}. {title}
 					<button className="btn btn-danger" onClick={()=>handleDelete(id)}>delete event</button>
 				</li>
-				
-			  </>
 		  ))}
 	  </ul>
 	  <button className="btn btn-info mt-3" onClick={addEvent}>Add Event</button>
