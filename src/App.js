fe257abd62b69f6fb93939971f5c0c7805fbc053
musafir928 <uganoghli@gmail.com>
2022-03-18 15:52:40 +0100

    master cleanup for new projects

--- src/App.js
+++ src/App.js
@@ -1,65 +1,8 @@
-import { useState } from "react"
 import "./App.scss"
-import Modal from "./components/Modal"
-import NewEventForm from "./components/NewEventForm"
-import NewEventsUseRef from "./components/NewEventsUseRef"
-import Title from "./components/Title"
 
 function App() {
-    const [showTitle, setShowTitle] = useState(true)
-    const [showModal, setShowModal] = useState(false)
-    const [events, setEvents] = useState([])
-
-    const handleDelete = (i) => setEvents((p) => p.filter((e) => e.id !== i))
-    const toggleTitle = () => setShowTitle((p) => !p)
     return (
-        <div className='App container'>
-            <div className='button-container d-flex justify-content-around'>
-                <button className='btn btn-primary mb-5' onClick={toggleTitle}>
-                    {showTitle ? "hide title" : "show title"}
-                </button>
-            </div>
-            {showTitle && (
-                <>
-                    <Title
-                        title='Mario Kingdom Events'
-                        subtitle='All the latest events in mario kingdom'
-                    />
-                    <Title />
-                </>
-            )}
-            <ul className='list-group'>
-                {events.map(({ title,date, location, id }, i) => (
-                    <li
-                        className='list-group-item row'
-                        key={i + 1}
-                    >
-                        <p className="col-3">{title}</p>
-                        <p className="col-3">{date}</p>
-                        <p className="col-3">{location}</p>
-                        <button
-                            className='btn btn-danger col-3'
-                            onClick={() => handleDelete(id)}
-                        >
-                            delete event
-                        </button>
-                    </li>
-                ))}
-            </ul>
-            <button className='btn btn-info mt-3' onClick={()=>setShowModal(true)}>
-                Add Event
-            </button>
-            {showModal && (
-                <Modal >
-                    <NewEventForm setShowModal={setShowModal} setEvents={setEvents}/>
-                </Modal>
-            )}
-            {/* {showModal && (
-                <Modal >
-                    <NewEventsUseRef setShowModal={setShowModal} setEvents={setEvents}/>
-                </Modal>
-            )} */}
-        </div>
+        <h1>clean up</h1>
     )
 }
 
