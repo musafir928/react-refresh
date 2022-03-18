199ba8df664424ed9d5b560a527df999e28ab472
musafir928 <uganoghli@gmail.com>
2022-03-18 20:15:03 +0100

    fetch data

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import "./App.scss"
+import TripList from './components/TripList'
 
 function App() {
     return (
-        <h1>clean up</h1>
+        <div className="APP">
+            <TripList />
+        </div>
     )
 }
 
